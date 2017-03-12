0fba8a293f45f9848283d6a92a080f8a7f050882
Andrew Sun <0xandrewsun@gmail.com>
2017-03-12 16:06:37 -0500

    Block the "upgrade to SoundCloud Go" popups

--- src/com/crossbowffs/soundcloudadaway/Hook.java
+++ src/com/crossbowffs/soundcloudadaway/Hook.java
@@ -1,50 +1,28 @@
 package com.crossbowffs.soundcloudadaway;
 
-import android.app.Notification;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
-import de.robv.android.xposed.IXposedHookLoadPackage;
-import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedBridge;
-import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.*;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 import java.io.File;
+import java.lang.reflect.Method;
 
 public class Hook implements IXposedHookLoadPackage {
     private static String getPackageVersion(XC_LoadPackage.LoadPackageParam lpparam) {
-        Class<?> parserCls = XposedHelpers.findClass("android.content.pm.PackageParser", lpparam.classLoader);
-        Object parser;
         try {
-            parser = parserCls.newInstance();
-        } catch (InstantiationException e) {
-            return null;
-        } catch (IllegalAccessException e) {
+            Class<?> parserCls = XposedHelpers.findClass("android.content.pm.PackageParser", lpparam.classLoader);
+            Object parser = parserCls.newInstance();
+            File apkPath = new File(lpparam.appInfo.sourceDir);
+            Object pkg = XposedHelpers.callMethod(parser, "parsePackage", apkPath, 0);
+            String versionName = (String)XposedHelpers.getObjectField(pkg, "mVersionName");
+            int versionCode = XposedHelpers.getIntField(pkg, "mVersionCode");
+            return String.format("%s (%d)", versionName, versionCode);
+        } catch (Throwable e) {
             return null;
         }
-        File apkPath = new File(lpparam.appInfo.sourceDir);
-        Object pkg = XposedHelpers.callMethod(parser, "parsePackage", apkPath, 0);
-        String versionName = (String)XposedHelpers.getObjectField(pkg, "mVersionName");
-        int versionCode = XposedHelpers.getIntField(pkg, "mVersionCode");
-        return String.format("%s (%d)", versionName, versionCode);
-    }
-
-    private static String urnToString(Object urn) {
-        return (String)XposedHelpers.getObjectField(urn, "content");
-    }
-
-    private static Object getAudioAdUrn(Object audioAd) {
-        return XposedHelpers.callMethod(audioAd, "getUrn");
-    }
-
-    private static Object getVideoAdUrn(Object videoAd) {
-        return XposedHelpers.callMethod(videoAd, "getAdUrn");
-    }
-
-    private static boolean isStreamAd(Object streamItem) {
-        return
-            (Boolean)XposedHelpers.callMethod(streamItem, "isAd") ||
-            (Boolean)XposedHelpers.callMethod(streamItem, "isPromoted") ||
-            (Boolean)XposedHelpers.callMethod(streamItem, "isUpsell");
     }
 
     private static void printInitInfo(XC_LoadPackage.LoadPackageParam lpparam) {
@@ -57,109 +35,156 @@ public class Hook implements IXposedHookLoadPackage {
         Xlog.i("SoundCloud AdAway version: %s (%d)", BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE);
     }
 
+    private static void findAndHookMethod(String className, ClassLoader classLoader, String methodName, Object... parameterTypesAndCallback) {
+        Xlog.i("Hooking %s#%s()", className, methodName);
+        try {
+            XposedHelpers.findAndHookMethod(className, classLoader, methodName, parameterTypesAndCallback);
+        } catch (Throwable e) {
+            Xlog.e("Failed to hook %s#%s()", className, methodName, e);
+        }
+    }
+
+    private static boolean isStreamAd(Object streamItem) {
+        return (Boolean)XposedHelpers.callMethod(streamItem, "isAd") ||
+               (Boolean)XposedHelpers.callMethod(streamItem, "isPromoted") ||
+               (Boolean)XposedHelpers.callMethod(streamItem, "isUpsell");
+    }
+
+    private static void blockPlayQueueAds(XC_LoadPackage.LoadPackageParam lpparam) {
+        // Block audio and video ads during playback
+        findAndHookMethod(
+            "com.soundcloud.android.ads.AdsOperations", lpparam.classLoader,
+            "insertAudioAd",
+            "com.soundcloud.android.playback.TrackQueueItem",
+            "com.soundcloud.android.ads.ApiAudioAd",
+            new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    Xlog.i("Blocked audio ad");
+                    param.setResult(null);
+                }
+            });
+
+        findAndHookMethod(
+            "com.soundcloud.android.ads.AdsOperations", lpparam.classLoader,
+            "insertVideoAd",
+            "com.soundcloud.android.playback.TrackQueueItem",
+            "com.soundcloud.android.ads.ApiVideoAd",
+            new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    Xlog.i("Blocked video ad");
+                    param.setResult(null);
+                }
+            });
+    }
+
+    private static void blockStreamAds(XC_LoadPackage.LoadPackageParam lpparam) {
+        // Block stream ads (e.g. promoted tracks, "install this app")
+        findAndHookMethod(
+            "com.soundcloud.android.stream.StreamAdapter", lpparam.classLoader,
+            "addItem",
+            int.class,
+            "com.soundcloud.android.stream.StreamItem",
+            new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    if (isStreamAd(param.args[1])) {
+                        Xlog.i("Blocked stream ad");
+                        param.setResult(null);
+                    }
+                }
+            });
+
+        findAndHookMethod(
+            "com.soundcloud.android.stream.StreamAdapter", lpparam.classLoader,
+            "addItem",
+            "com.soundcloud.android.stream.StreamItem",
+            new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    if (isStreamAd(param.args[0])) {
+                        Xlog.i("Blocked stream ad");
+                        param.setResult(null);
+                    }
+                }
+            });
+    }
+
+    private static void blockAppboyPushNotifications(XC_LoadPackage.LoadPackageParam lpparam) {
+        // Block Appboy push notifications (no, the notification settings
+        // aren't enough)
+        findAndHookMethod(
+            "com.appboy.AppboyGcmReceiver", lpparam.classLoader,
+            "onReceive",
+            Context.class,
+            Intent.class,
+            XC_MethodReplacement.DO_NOTHING);
+    }
+
+    private static void blockAppboyInAppMessages(XC_LoadPackage.LoadPackageParam lpparam) {
+        // Block Appboy in-app messages. An example is the "upgrade to
+        // SoundCloud Go" popup that gets displayed when the app starts.
+        findAndHookMethod(
+            "com.soundcloud.android.analytics.appboy.RealAppboyWrapper", lpparam.classLoader,
+            "registerInAppMessageManager",
+            Activity.class,
+            XC_MethodReplacement.DO_NOTHING);
+
+        findAndHookMethod(
+            "com.soundcloud.android.analytics.appboy.RealAppboyWrapper", lpparam.classLoader,
+            "unregisterInAppMessageManager",
+            Activity.class,
+            XC_MethodReplacement.DO_NOTHING);
+    }
+
+    private static void enableOfflineContent(XC_LoadPackage.LoadPackageParam lpparam) {
+        // Not sure if this actually does anything useful.
+        // Supposedly it enables listening to tracks while offline,
+        // but I can do that without this anyways, so...
+        findAndHookMethod(
+            "com.soundcloud.android.configuration.FeatureOperations", lpparam.classLoader,
+            "isOfflineContentEnabled",
+            XC_MethodReplacement.returnConstant(true));
+    }
+
+    private static void enableDeveloperMode(XC_LoadPackage.LoadPackageParam lpparam) {
+        // This gives you a cool developer menu if you slide from the right edge.
+        // It's not actually useful for anything though (no, you can't upgrade
+        // to Go for free, I've tried)
+        findAndHookMethod(
+            "com.soundcloud.android.configuration.FeatureOperations", lpparam.classLoader,
+            "isDevelopmentMenuEnabled",
+            XC_MethodReplacement.returnConstant(true));
+
+        // The return type is RxJava Observable<Boolean>, which is ProGuarded.
+        // We can reliably find the return type from the method itself.
+        try {
+            Class<?> featureOpsCls = XposedHelpers.findClass("com.soundcloud.android.configuration.FeatureOperations", lpparam.classLoader);
+            Method devMenuEnabledMethod = XposedHelpers.findMethodExact(featureOpsCls, "developmentMenuEnabled");
+            Class<?> observableCls = devMenuEnabledMethod.getReturnType();
+            Object observableTrue = XposedHelpers.callStaticMethod(observableCls, "just", new Class<?>[] {Object.class}, true);
+            XposedBridge.hookMethod(devMenuEnabledMethod, XC_MethodReplacement.returnConstant(observableTrue));
+        } catch (Throwable e) {
+            Xlog.e("Failed to enable developer mode", e);
+        }
+    }
+
     @Override
-    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         if (!"com.soundcloud.android".equals(lpparam.packageName)) {
             return;
         }
 
-        // Possibly relevant classes:
-        // AdsController
-        // AdsOperations
-        // AdFunctions
-        // PlayQueueManager
-        // PlayQueue
-        // PlayQueueItem
-
         printInitInfo(lpparam);
 
-        try {
-            Xlog.i("Hooking AdsOperations.insertAudioAd()...");
-            XposedHelpers.findAndHookMethod(
-                "com.soundcloud.android.ads.AdsOperations", lpparam.classLoader,
-                "insertAudioAd",
-                "com.soundcloud.android.playback.TrackQueueItem",
-                "com.soundcloud.android.ads.ApiAudioAd",
-                new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        Object urn = getAudioAdUrn(param.args[1]);
-                        String urnStr = urnToString(urn);
-                        Xlog.i("Blocked audio ad: %s", urnStr);
-                        param.setResult(null);
-                    }
-                });
+        blockPlayQueueAds(lpparam);
+        blockStreamAds(lpparam);
+        blockAppboyPushNotifications(lpparam);
+        blockAppboyInAppMessages(lpparam);
+        // enableOfflineContent(lpparam);
+        // enableDeveloperMode(lpparam);
 
-            Xlog.i("Hooking AdsOperations.insertVideoAd()...");
-            XposedHelpers.findAndHookMethod(
-                "com.soundcloud.android.ads.AdsOperations", lpparam.classLoader,
-                "insertVideoAd",
-                "com.soundcloud.android.playback.TrackQueueItem",
-                "com.soundcloud.android.ads.ApiVideoAd",
-                new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        Object urn = getVideoAdUrn(param.args[1]);
-                        String urnStr = urnToString(urn);
-                        Xlog.i("Blocked video ad: %s", urnStr);
-                        param.setResult(null);
-                    }
-                });
-
-            Xlog.i("Hooking StreamAdapter.addItem(int, StreamItem)...");
-            XposedHelpers.findAndHookMethod(
-                "com.soundcloud.android.stream.StreamAdapter", lpparam.classLoader,
-                "addItem",
-                int.class,
-                "com.soundcloud.android.stream.StreamItem",
-                new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        if (isStreamAd(param.args[1])) {
-                            Xlog.i("Blocked stream ad");
-                            param.setResult(null);
-                        }
-                    }
-                });
-
-            Xlog.i("Hooking StreamAdapter.addItem(StreamItem)...");
-            XposedHelpers.findAndHookMethod(
-                "com.soundcloud.android.stream.StreamAdapter", lpparam.classLoader,
-                "addItem",
-                "com.soundcloud.android.stream.StreamItem",
-                new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        if (isStreamAd(param.args[0])) {
-                            Xlog.i("Blocked stream ad");
-                            param.setResult(null);
-                        }
-                    }
-                });
-
-            // Technically this can be done in a better way by disabling the
-            // GCM receiver that calls this method in the first place, but that
-            // requires access to a Context object and I don't feel like finding
-            // a good way to obtain one.
-            Xlog.i("Hooking NotificationManagerCompat.notify()...");
-            XposedHelpers.findAndHookMethod(
-                "android.support.v4.app.NotificationManagerCompat", lpparam.classLoader,
-                "notify", String.class, int.class, Notification.class,
-                new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        String tag = (String)param.args[0];
-                        if ("appboy_notification".equals(tag)) {
-                            Xlog.i("Blocked Appboy notification");
-                            param.setResult(null);
-                        }
-                    }
-                });
-        } catch (Throwable e) {
-            Xlog.e("Exception occurred while hooking methods", e);
-            throw e;
-        }
-
-        Xlog.i("SoundCloud AdAway init successful!");
+        Xlog.i("SoundCloud AdAway initialization complete!");
     }
 }
